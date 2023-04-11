046b8434497d88573e2eaa6c213501f3d85fd16f
petrushinmd <petrushinmd@ya.ru>
2023-04-11 16:17:30 +0300

    added edit button

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,9 +1,11 @@
 import './style'
+import Button from '../Button/Button';
 
 export default function Card({ movie }) {
     return (
         <div className='card'>
             <img src={movie.url} />
+            <Button className="edit" text="⋮" />
             <div className='description'>
                 <div className='year'>{movie.year}</div>
                 <p className='title'>{movie.name}</p>
