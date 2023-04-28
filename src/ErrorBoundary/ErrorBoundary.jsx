ab54b9497463f7804233c3e05be4db9d0c88dd6c
petrushinmd <petrushinmd@ya.ru>
2023-04-28 08:21:47 +0300

    changed error boundary styles

--- src/ErrorBoundary/ErrorBoundary.jsx
+++ src/ErrorBoundary/ErrorBoundary.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './style';
 
 class ErrorBoundary extends React.Component {
     constructor(props) {
@@ -16,7 +17,7 @@ class ErrorBoundary extends React.Component {
 
     render() {
         if (this.state.hasError) {
-            return <h1>Something went wrong</h1>;
+            return <h1 className="error-message">Something went wrong ☹</h1>;
         }
 
         return this.props.children;
